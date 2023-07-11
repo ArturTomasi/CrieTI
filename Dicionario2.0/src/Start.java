2ece05b5f15b508f6681a2634b292cd3adef1ae3
Artur Tomasi <art@interact.com.br>
2023-07-10 21:03:30 -0300

    Adicionado dicionario 2.0 com telas e banco

---
+++ Dicionario2.0/src/Start.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author art
+ */
+public class Start
+{
+    public static void main( String[] args )
+    {
+        new telas.TelaPrincipal().setVisible( true );
+    }
+}
